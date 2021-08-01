dabf0b15388f4babe207b966b338a50cc938d917
hwangma27 <pikacu22@kakao.com>
2021-08-01 20:23:20 +0900

    [ADD] MAPPING COLUMNS USING ANNOTATION

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,40 +1,65 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
-//default name of table is class name
-//@Table(name = "tablename")
 public class Member {
 
     @Id
     private Long id;
 
-    private String name;
+    /*
+    ANNOTATION
 
-    public Member() {
+    insertable, updateable > default value is true
+    nullable > default value is true > false means null isn't allow to this column
+    unique > unique annotation barely used because key naming rule doesn't clear
+    */
+    @Column(name = "name", insertable = true,
+            updatable = true, nullable = false, unique = true)
+    private String username;
+
+    private Integer age;
+    /*
+    EnumType
+    OPTION 1 : ORDINAL(default) > INTEGER
+               insert order of enum data
+    OPTION 2 : STRING
+               insert enum data
+
+    WARNING
+    Using ORDINAL option
+        when data order changed or insert extra enum data
+        it can cause disaster.
+    */
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    //support java8
+    //using dateTime without annotation
+    private LocalDateTime testLocalTime;
+    //variable type is String then make column type as clob
+    @Lob
+    private String description;
+
+    //not mapping with database table
+    @Transient
+    private  int temp;
+
+    public Member(){
     }
 
-    public Member(Long id, String name) {
+    public Member(long id, String username) {
         this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.username = username;
     }
 }
