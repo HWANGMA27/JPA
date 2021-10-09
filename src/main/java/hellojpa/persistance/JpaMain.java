c875e1291cae24907767d58f375bef7f11210b48
hwangma27 <pikacu22@kakao.com>
2021-10-09 23:48:59 +0900

    [ADD] 2WAYS TO AVOID ENTITY VALUE SHARING PROBLEM

--- src/main/java/hellojpa/persistance/JpaMain.java
+++ src/main/java/hellojpa/persistance/JpaMain.java
@@ -18,11 +18,31 @@ public class JpaMain {
 
         tx.begin();
         try {
+            Address address = new Address("city", "street", "zipcode");
             Member2 member = new Member2();
             member.setUsername("test");
-            member.setHomeAdress(new Address("city", "street", "zipcode"));
+            member.setHomeAdress(address);
             member.setWorkPeriod(new Period());
             em.persist(member);
+            //entity value type shouldn't be share
+            //not making setter can be solution of this problem
+            //or declare access modifier as private
+
+            //Sol1 copy value
+            Address newAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member2 member2 = new Member2();
+            member2.setUsername("test2");
+            member2.setHomeAdress(newAddress);
+            member2.setWorkPeriod(new Period());
+            em.persist(member2);
+
+            //Sol2 make new entity
+            Address newAddress2 = new Address("new city", address.getStreet(), address.getZipcode());
+            member2.setHomeAdress(newAddress2);
+
+
+            //member.getHomeAdress().setCity("newCity");
             tx.commit();
         }catch (Exception e){
             tx.rollback();
