d8b4342ed357e7cc1de5efc563c1c7f9f79a62c8
hwangma27 <pikacu22@kakao.com>
2021-12-03 19:28:02 +0900

    [ADD] Entity Join Using JPQL

--- src/main/java/JpqlMain.java
+++ src/main/java/JpqlMain.java
@@ -1,4 +1,5 @@
 import jpql.Member;
+import jpql.Team;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,24 +13,24 @@ public class JpqlMain {
 
         tx.begin();
         try {
-            for(int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("HELLO"+i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("A");
+            Member member = new Member();
+            member.setUsername("HELLO");
+            member.setAge(9);
+            member.setTeam(team);
+            em.persist(team);
+            em.persist(member);
+
             em.flush();
             em.clear();
 
             //entity call by jpql also managed by persistence context
-            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(50)
-                    .setMaxResults(100)
+            //하이버네이터 5.1부터 연관관계가 없는 엔티티간의 조인도 지원한다.
+            String query = "select m from Member m join m.team t on t.name = 'A' ";
+            List<Member> list = em.createQuery(query, Member.class)
                     .getResultList();
-            System.out.println("resultList.size() = " + resultList.size());
-            for(Member temp : resultList){
-                System.out.println(temp.toString());
-            }
+            System.out.println(list.size());
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/jpql/Team.java
+++ src/main/java/jpql/Team.java
@@ -11,6 +11,17 @@ public class Team {
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
     public Long getId() {
         return id;
     }
