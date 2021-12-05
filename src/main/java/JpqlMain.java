088690399f9dad28759818400bd0c83ad8293461
hwangma27 <pikacu22@kakao.com>
2021-12-05 14:58:28 +0900

    [ADD] Path expression and Implicit join

--- src/main/java/JpqlMain.java
+++ src/main/java/JpqlMain.java
@@ -34,10 +34,21 @@ public class JpqlMain {
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) from Member m";
-            List<String> resultList = em.createQuery(query, String.class).getResultList();
-            for(String temp : resultList){
-                System.out.println(temp);
+            /*묵시적 내부 조인 발생
+              눈에 보이지 않는 조인이 실제 쿼리가 날라갈 때 발상함으로 나중에 쿼리를 추적할때 시간이 걸리고 튜닝시에도 어려움이 발생한다.
+              실무에서는 사용하지 않는것을 권장한다.
+            */
+            String query = "select m.team from Member m";
+
+            /*명시적 조인
+              가능하면 명시적 조인으로 눈에 보이게 작성해야 한다.
+              명시적 조인을 하면 별칭을 얻어서 다시 경로 탐색을 시작할 수 있다는 장점이 있다.
+            */
+            String query2 = "select t from Member m inner join m.team t";
+
+            List<Team> resultList = em.createQuery(query2, Team.class).getResultList();
+            for(Team temp : resultList){
+                System.out.println(temp.getName());
             }
             tx.commit();
         }catch (Exception e){
