96bee21d5b956d26834433ed29d097735f530c6f
hwangma27 <pikacu22@kakao.com>
2022-01-03 14:58:19 +0900

    [ADD] Spring Data JPA 활용한 Member 검색

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,41 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    //파라미터가 길어지면 메소드명도 길어지는 단점이 있다.
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    //네임드 쿼리가 최우선 순위로 호출됨
+    List<Member> findByUsername(@Param("username") String username);
+
+    /**
+     * 조건이 많아져도 메소드명은 그대로라는 장점이 있다.
+     * 실무에서 많이 사용
+     * 또다른 장점으로는 쿼리에 오류가 있다면, 컴파일단에서 오류가 발생한다.
+     */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUserNameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    //in으로 조회
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username);
+    Member findMemberByUsername(String username);
+    Optional<Member> findOptionalByUsername(String username);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,15 +1,21 @@
 package study.datajpa.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import javax.persistence.EntityManager;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,10 +24,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
 class MemberRepositoryTest {
 
     @Autowired private MemberRepository memberRepository;
+    @Autowired private TeamRepository teamRepository;
 
     @Test
     public void test() throws Exception{
@@ -74,5 +80,92 @@ class MemberRepositoryTest {
         List<Member> findMemberList = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
         //then
         assertThat(findMemberList.size()).isEqualTo(1);
+    }
+    
+    @Test
+    public void testNamedQuery() throws Exception{
+        //given
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> findMemberList = memberRepository.findByUsername("AAA");
+        //then
+        assertThat(findMemberList.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testQuery() throws Exception{
+        //given
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> findMemberList = memberRepository.findUser("AAA", 10);
+        //then
+        assertThat(findMemberList.get(0)).isEqualTo(member1);
+    }
+
+    @Test
+    public void testUsernameList(){
+        //given
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<String> usernameList = memberRepository.findUserNameList();
+        //then
+        for (String name: usernameList) {
+            System.out.println("name : " + name);
+        }
+    }
+
+
+    @Test
+    public void testMemberTeam(){
+        //given
+        Team team1 = new Team("teamA");
+        teamRepository.save(team1);
+        Member member1 = new Member("AAA", 10);
+        member1.setTeam(team1);
+        memberRepository.save(member1);
+
+        //when
+        List<MemberDto> dtoList = memberRepository.findMemberDto();
+        //then
+        for (MemberDto memberDto: dtoList) {
+            System.out.println(memberDto);
+        }
+    }
+
+    @Test
+    public void testByNames(){
+        //given
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        //then
+        for (Member member: usernameList) {
+            System.out.println("name : " + member.getUsername());
+        }
+    }
+
+    @Test
+    public void returnTypeTest(){
+        //given
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> listMember = memberRepository.findListByUsername("AAA");
+        Member member = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> optionalMember = memberRepository.findOptionalByUsername("AAA");
     }
 }
