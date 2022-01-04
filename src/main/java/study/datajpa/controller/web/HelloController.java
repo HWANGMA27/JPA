c26c89fb0d0dcf3cca447135dd266aa191977e48
hwangma27 <pikacu22@kakao.com>
2022-01-04 17:01:35 +0900

    [ADD] Spring Data JPA 도메인 클래스 컨버터

--- src/main/java/study/datajpa/controller/web/HelloController.java
+++ src/main/java/study/datajpa/controller/web/HelloController.java
@@ -1,4 +1,4 @@
-package study.datajpa.controller;
+package study.datajpa.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
