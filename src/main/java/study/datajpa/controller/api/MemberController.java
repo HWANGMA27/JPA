4c7abd74d26ffb713b0e27338003a95a155deb4d
hwangma27 <pikacu22@kakao.com>
2022-01-05 14:50:57 +0900

    [ADD] JPA ID 생성 전략이 @GeneratedValue가 아닐 경우, Meger가 아닌 Persist를 사용하도록 구현

--- src/main/java/study/datajpa/controller/api/MemberController.java
+++ src/main/java/study/datajpa/controller/api/MemberController.java
@@ -36,7 +36,7 @@ public class MemberController {
         return page.map(MemberDto::new);
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init(){
         for (int i = 0; i < 100; i++) {
             memberRepository.save(new Member("user"+1, i));
